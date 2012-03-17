acceca042d8ef16364736d417dc3a2cf35152d5b
britg <brit@britg.com>
2012-03-17 09:27:20 -0500

    Deactivate the corona after 5s, but don't deactivate the corona during a running streak.

--- Assets/Scripts/Fireball/Collector.js
+++ Assets/Scripts/Fireball/Collector.js
@@ -1,18 +1,43 @@
 
 private var thisTransform : Transform;
 
-private var startScale : Vector3 = Vector3(10, 9, 9);
+private var startScale : Vector3 = Vector3(7, 6, 6);
 private var isActive : boolean = false;
+
+private var currActiveTime : float = 0.0;
+var activeTime : float = 5.0;
+
+private var fireball : GameObject;
+private var streak : Streak;
 
 function Start() {
 	thisTransform = transform;
 	SendMessage("Update");
 	NotificationCenter.DefaultCenter().AddObserver(this, Notifications.STREAK_LEVEL_CHANGED);
 	NotificationCenter.DefaultCenter().AddObserver(this, Notifications.POWERDOWN);
+	
+	fireball = GameObject.Find("Fireball");
+	streak = fireball.GetComponent("Streak");
 }
 
 function Update () {
+	Rotate();
+	
+	if(isActive) {
+		CheckTimeout();
+	}
+}
+
+function Rotate() {
 	thisTransform.Rotate(Time.deltaTime*140, 0, 0);
+}
+
+function CheckTimeout() {
+	currActiveTime += Time.deltaTime;
+
+	if(currActiveTime >= activeTime && !streak.ongoing) {
+		Deactivate();
+	}
 }
 
 function OnStreakLevelChange (notification : Notification) {
@@ -34,5 +59,6 @@ function Activate () {
 
 function Deactivate() {
 	isActive = false;
+	currActiveTime = 0.0;
 	thisTransform.localScale = Vector3(0, 0, 0);
 }
