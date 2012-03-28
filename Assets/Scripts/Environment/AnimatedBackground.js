11e165b3f9d870c6f5d5cc12787d950245d0ff61
britg <brit@britg.com>
2012-03-27 22:14:03 -0500

    Sorcerer's forest background

--- Assets/Scripts/Environment/AnimatedBackground.js
+++ Assets/Scripts/Environment/AnimatedBackground.js
@@ -1,8 +1,8 @@
 
-var Level : GameObject;
 var factor : float = 0.1;
 
 private var offset : Vector2;
+private var Level : GameObject;
 private var scrolling : Scroller;
 
 private var thisRenderer : Renderer;
@@ -10,6 +10,8 @@ private var thisMaterial : Material;
 
 
 function Start () {
+	Level = GameObject.Find("Level");
+	
 	thisRenderer = renderer;
 	thisMaterial = thisRenderer.material;
 	offset = renderer.material.GetTextureOffset("_MainTex");
